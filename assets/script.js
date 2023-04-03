0fc0d8bbf628109d7f22d220e3ee1d75da05ebaa
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-04-03 11:19:39 -0700

    Shrink video files

--- assets/script.js
+++ assets/script.js
@@ -4,28 +4,34 @@ const whiteOut = document.querySelector(".white-out")
 const timeBox = document.querySelector(".add-flight-time")
 const flightHours = document.querySelector("#add-hours")
 const flightMinutes = document.querySelector("#add-minutes")
+const flightDate = document.querySelector("#add-date")
 const submitFlightText = document.querySelector("#log-flight-btn")
-const totalHours = document.querySelector(".totalHours")
-const totalMinutes = document.querySelector(".totalMinutes")
+const totalHours = document.querySelector(".total-hours")
+const totalMinutes = document.querySelector(".total-minutes")
 
 let fHours = 0
 let fMinutes = 0
+let fDate = ""
+
 
 logTime.addEventListener("click", () => {
     timeBox.style.display = "flex";
     whiteOut.style.display = "block";
-
     submitFlightText.addEventListener("click", () => {
-        // debugger;
         fHours = flightHours.value;
         fMinutes = flightMinutes.value;
+        fDate = flightDate.value;
         console.log(fHours);
         localStorage.setItem("Hours", fHours)
         localStorage.setItem("Minutes", fMinutes)
+        localStorage.setItem("Date", fDate)
         timeBox.style.display = "none";
         whiteOut.style.display = "none";
+        addTotal()
     })
-    addTotal()
+
+
+
 
 })
 
@@ -36,3 +42,4 @@ function addTotal() {
     // hours = hours + 
 
 }
+
