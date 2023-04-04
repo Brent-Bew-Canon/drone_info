2c48cd5fd04e11291ca916ebd87aad0a764a3aa4
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-04-04 16:47:15 -0700

    color coding pilot level based on flight time

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@ function init() {
     let initMin = localStorage.getItem("Minutes")
     let lastHour = localStorage.getItem("Last-Hour")
     let lastMinute = localStorage.getItem("Last-Min")
+    //only change the html if there is something from the local storage
     if (initHrs > 0 || initMin > 0) {
         totalHours.textContent = localStorage.getItem("Hours")
         totalMinutes.textContent = localStorage.getItem("Minutes")
@@ -32,6 +33,7 @@ function init() {
 init()
 
 //FIXME - runs function twice. Used a counter as a temp fix
+
 logTime.addEventListener("click", () => {
     event.preventDefault()
     let counter = 0
@@ -91,8 +93,39 @@ function displayTotal(hrs, min, date, lastHr, lastMin) {
     document.querySelector("#last-time").textContent = lastHr + "hrs " + lastMin + "min"
 }
 
+//resets input values to zero
 function resetValues() {
     flightHours.value = 0;
     flightMinutes.value = 0;
 }
+
+// FIXME not writing pilot level to doc
+function pilotLevel() {
+    if (Number(localStorage.getItem("Hours") >= 100)) {
+        for (let i = 2; i < 6; i++) {
+            let dot = $(".dot" + i)
+            dot.attr("style", "background-color: rgb(77, 193, 255)")
+        }
+        $(".height-span2").val("master")
+    } else if (Number(localStorage.getItem("Hours") >= 50)) {
+        for (let i = 2; i < 5; i++) {
+            let dot = $(".dot" + i)
+            dot.attr("style", "background-color: rgb(77, 193, 255)")
+        }
+        $(".height-span2").val("exxcleent")
+    } else if (Number(localStorage.getItem("Hours") >= 20)) {
+        for (let i = 2; i < 4; i++) {
+            let dot = $(".dot" + i)
+            dot.attr("style", "background-color: rgb(77, 193, 255)")
+        }
+        $(".height-span2").val("greart guy")
+    } else if (Number(localStorage.getItem("Hours") >= 10)) {
+        let i = 2
+        $(".height-span2").val("Cool guy")
+        let dot = $(".dot" + i)
+        dot.attr("style", "background-color: rgb(77, 193, 255)")
+    }
+}
+
+pilotLevel()
 
